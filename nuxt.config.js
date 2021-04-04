c93cf134c473780a0a868e63a40aa61783385f69
Marrion Lulu <marrion91300@gmail.com>
2021-04-04 00:27:45 -0700

    Using generate for deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,5 +75,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend (config, ctx) {
+      config.module.rules.forEach((rule) => {
+        if (rule.test.toString() === '/\\.vue$/') {
+          rule.options.optimizeSSR = false
+        }
+      })
+    }
   }
 }
